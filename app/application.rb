f76a2e7f30e1b143a55e3ee2794150703f6bc210
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 18:56:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     time = Time.new 
-    if time.hour <= 24
-      resp.write "Good Morning!"
-    else 
+    if time.hour > 11
       resp.write "Good Afternoon!"
+    else 
+    
+     resp.write "Good Morning!"
     end
     resp.finish 
   end 

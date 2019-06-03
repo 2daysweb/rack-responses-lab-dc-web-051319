8813d719bdf66c325c20a54ccf26da40cb435dba
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 19:02:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     time = Time.new 
-    puts time 
-    puts time.hour 
+
     if (time.hour > 11)
       resp.write "Good Afternoon!"
     else
